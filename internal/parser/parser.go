7b42537ef697ad465eaa2306a669ac0e70bd4756
Yeabsira Abate Guangul <k-yguangu@c1r1p5.hive.fi>
2026-07-09 13:10:11 +0300

    add MapParser and Categorize stations & connections

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,49 @@
 package parser
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func MapParser(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("map file dosn't exist")
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		eachLine := scanner.Text()
+		beforeComment, _, _ := strings.Cut(eachLine, "#")
+		trimmedLine := strings.TrimSpace(beforeComment)
+		if trimmedLine == "" {
+			continue
+		}
+		lines = append(lines, trimmedLine)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading map file: %w", err)
+	}
+	return lines, nil
+}
+
+func Categorize(lines []string) (stations []string, connections []string, err error) {
+	stationsIdx := slices.Index(lines, "stations:")
+	connectionsIdx := slices.Index(lines, "connections:")
+
+	if stationsIdx == -1 {
+		return nil, nil, fmt.Errorf("missing 'stations:' section")
+	}
+	if connectionsIdx == -1 {
+		return nil, nil, fmt.Errorf("missing 'connections:' section")
+	}
+	stations = append(stations, lines[stationsIdx+1:connectionsIdx]...)
+	connections = append(connections, lines[connectionsIdx+1:]...)
+	return stations, connections, nil
+
+}
