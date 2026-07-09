e26d78df96fa24bbf467bcceee69234ee7b36f31
Yeabsira Abate Guangul <k-yguangu@c1r1p5.hive.fi>
2026-07-09 17:11:57 +0300

    add station validator

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -4,9 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"slices"
+	"strconv"
 	"strings"
 )
+type Station struct {
+	Name string
+	X    int
+	Y    int
+}
 
 func MapParser(path string) ([]string, error) {
 	file, err := os.Open(path)
@@ -14,7 +21,7 @@ func MapParser(path string) ([]string, error) {
 		return nil, fmt.Errorf("map file dosn't exist")
 	}
 	defer file.Close()
-
+	
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -32,7 +39,53 @@ func MapParser(path string) ([]string, error) {
 	return lines, nil
 }
 
+var nameRule = regexp.MustCompile(`^[a-z0-9_]+$`)
+
+func Stationvalidator (stationLines []string) (map[string] *Station, error){
+	stations := make(map[string]*Station)
+	coords := make(map[[2]int]string)
+
+	for _, line := range stationLines{
+		eachparts := strings.Split(line, ",")
+		if len(eachparts) != 3 {
+			return nil, fmt.Errorf("invalid station line: %s", line)
+		}
+		name:= strings.TrimSpace(eachparts[0])
+		xCoord:= strings.TrimSpace(eachparts[1])
+		yCoord:= strings.TrimSpace(eachparts[2])
+
+		if !nameRule.MatchString(name){
+			return nil,fmt.Errorf("invalid staion name: %s", name)
+		}
+		_, exists := stations[name]
+		if exists{
+			return nil, fmt.Errorf("duplicate station name: %s", name)
+		}
+
+		x, err := strconv.Atoi(xCoord)
+		if err != nil || x < 0{
+			return nil, fmt.Errorf("error changing x coordinate for %s", xCoord)
+		}
+
+		y, err := strconv.Atoi(yCoord)
+		if err != nil || y < 0{
+			return nil, fmt.Errorf("error changing y coordinate for %s", yCoord)
+		}
+
+		coord := [2]int{x,y}
+		another, exists:= coords[coord]
+		if exists{
+			return nil, fmt.Errorf("coordinates duplicated %s & %s", name, another)
+		}
+		coords[coord] = name
+
+		stations[name] = &Station{Name: name, X:x, Y:y}
+	}
+	return stations , nil
+}
+
 func Categorize(lines []string) (stations []string, connections []string, err error) {
+
 	stationsIdx := slices.Index(lines, "stations:")
 	connectionsIdx := slices.Index(lines, "connections:")
 
