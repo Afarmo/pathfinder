71b6a90b9dd1493a8ca906ad73ef49924f69caa1
Yeabsira Abate Guangul <k-yguangu@c1r1p3.hive.fi>
2026-07-15 14:20:56 +0300

    add test for categorize validation

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,9 +1,45 @@
 package parser
-import(
-	"os"
+
+import (
 	"slices"
+	"testing"
 )
 
-func TestCategorizeValid(t *testing.T){
-	
+func TestCategorizeValid(t *testing.T) {
+	lines := []string{
+		"stations:",
+		"waterloo,3,1",
+		"victoria,6,7",
+		"connections:",
+		"waterloo-victoria",
+	}
+
+	stationLine, connectionLine, err := Categorize(lines)
+	if err != nil {
+		t.Errorf("expected no error: got: %v", err)
+	}
+	wantedStationLine := []string{"waterloo,3,1", "victoria,6,7"}
+	wantedConnectionLine := []string{"waterloo-victoria"}
+	if !slices.Equal(wantedStationLine, stationLine) {
+		t.Errorf("expected: %+v, got: %+v", wantedStationLine, stationLine)
+	}
+	if !slices.Equal(wantedConnectionLine, connectionLine) {
+		t.Errorf("expected: %+v, got: %+v", wantedConnectionLine, connectionLine)
+	}
+}
+
+func TestCategorizeMissingStation(t *testing.T) {
+	lines := []string{"connections:", "waterloo-victoria"}
+	_, _, err := Categorize(lines)
+	if err == nil {
+		t.Fatal("expected error for missing stations section, got nil")
+	}
+}
+
+func TestCategorizeMissingConnections(t *testing.T) {
+	lines := []string{"stations:", "waterloo,3,1"}
+	_, _, err := Categorize(lines)
+	if err == nil {
+		t.Fatal("expected error for missing connections section, got nil")
+	}
 }
