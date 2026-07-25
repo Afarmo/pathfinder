1a6d4c1285f6ef0a98ee894c42870bebc016f4a8
Abderahman Afaryad <kallahti@hotmail.com>
2026-07-25 20:45:50 +0300

    Improve time complexity immensely and refactor to compute moves instead of simulating every turn

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -7,93 +7,94 @@ import (
 
 type Train struct {
 	ID       int
-	Position int
 	PathID   int
+	Position int
 }
 
 func Scheduler(paths [][]string, trainCount int) []string {
 
-	// Pre assign path lengths
+	// Pre-allocate path lengths
 	var pathLens = make([]int, len(paths))
 	for i := range paths {
-		pathLens[i] = len(paths[i]) - 1
+		pathLens[i] = len(paths[i])
 	}
 
+	// Assign trains to paths
 	var Trains = make([]*Train, trainCount)
-	// Assign ID's to trains
-	for i := range Trains {
-		Trains[i] = &Train{ID: i + 1}
+	pathTrainCount := make([]int, len(paths)) // trains in specific path
+
+	for trainIndex := range Trains {
+
+		bestPath := 0
+		bestArrival := 1<<31 - 1
+
+		for p := range paths {
+			arrival := pathLens[p] - 1 + pathTrainCount[p]
+			if arrival < bestArrival {
+				bestArrival = arrival
+				bestPath = p
+			}
+		}
+
+		Trains[trainIndex] = &Train{
+			ID:       trainIndex + 1,
+			PathID:   bestPath,
+			Position: pathTrainCount[bestPath],
+		}
+
+		pathTrainCount[bestPath]++
 	}
-	AssignTrains(Trains, paths)
-	var turns []string
-	var arrivedCount int
-	// Main loop that runs as long as all trains haven't arrived
-	for {
 
-		// var turn strings.Builder
-		occupied := map[string]bool{}
+	// Compute maximum turn by checking the longest arrival
+	var maxTurn int
+	for p := range paths {
+		if pathTrainCount[p] == 0 {
+			continue
+		}
+		arrival := pathLens[p] - 1 + pathTrainCount[p] - 1
+		if arrival > maxTurn {
+			maxTurn = arrival
+		}
+	}
+
+	// Compute all moves per turn
+	type Move struct {
+		TrainID int
+		Station string
+	}
+
+	moves := make([][]Move, maxTurn+1)
+
+	for _, train := range Trains {
+
+		for position := 1; position < pathLens[train.PathID]; position++ {
+			turn := train.Position + position
+			moves[turn] = append(moves[turn], Move{
+				TrainID: train.ID,
+				Station: paths[train.PathID][position],
+			})
+		}
+	}
+
+	// Construct all turns with moves
+	var turns = make([]string, 0, maxTurn+1)
+
+	for i := 0; i <= maxTurn; i++ {
+
 		var turn strings.Builder
-		var moved bool
-		for i := range Trains {
-			train := Trains[i]
-			// Train has already reached the destination
-			if train.Position == pathLens[train.PathID] {
-				arrivedCount++
-				continue
-			}
-			if train.Position < pathLens[train.PathID] {
-				moved = true
-			}
 
-			// Next station is full
-			if _, full := occupied[paths[train.PathID][train.Position+1]]; full == true {
-				continue
-			}
-			// Free current station
-			if train.Position > 0 && train.Position < pathLens[train.PathID] {
-				delete(occupied, paths[train.PathID][train.Position])
-			}
-			// Move train
-			train.Position++
-
+		for _, move := range moves[i] {
 			turn.WriteByte('T')
-			turn.WriteString(strconv.Itoa(train.ID))
+			turn.WriteString(strconv.Itoa(move.TrainID))
 			turn.WriteByte('-')
-			turn.WriteString(paths[train.PathID][train.Position])
+			turn.WriteString(move.Station)
 			turn.WriteByte(' ')
-
-			// Occupy the new station
-			if train.Position > 0 && train.Position < pathLens[train.PathID] {
-				occupied[paths[train.PathID][train.Position]] = true
-			}
 		}
 
 		if turn.Len() > 0 {
 			turns = append(turns, strings.TrimSuffix(turn.String(), " "))
-
-		}
-		if !moved {
-			break
 		}
 	}
+
 	return turns
-}
-
-func AssignTrains(trains []*Train, paths [][]string) {
-	counts := make([]int, len(paths))
-
-	for i := range trains {
-		bestPath := 0
-		bestArrival := 1<<31 - 1 // max int
-		for j, path := range paths {
-			arrival := len(path) - 1 + counts[j]
-			if arrival < bestArrival {
-				bestArrival = arrival
-				bestPath = j
-			}
-		}
-
-		trains[i].PathID = bestPath
-		counts[bestPath]++
-	}
 }
