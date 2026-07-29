df0223496a62785346f174338386cbff34b0d248
Abderahman Afaryad <kallahti@hotmail.com>
2026-07-29 18:09:21 +0300

    Change var's to const's and remove magenta

--- cmd/main.go
+++ cmd/main.go
@@ -11,26 +11,27 @@ import (
 	"pathfinder/internal/scheduler"
 )
 
-var Reset = "\033[0m"
-var Magenta = "\033[35m"
-var Red = "\033[31m"
+const (
+	Red   = "\033[31m"
+	Reset = "\033[0m"
+)
 
 func main() {
 	cfg, err := cli.ParseArgs(os.Args)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%sError Parsing Arguments: %v%s\n",Red, err, Reset)
+		fmt.Fprintf(os.Stderr, "%sError Parsing Arguments: %v%s\n", Red, err, Reset)
 		return
 	}
 
 	graph, err := parser.MapParser(cfg.FilePath, cfg.Start, cfg.End)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%sError Parsing Map: %v%s\n",Red, err, Reset)
+		fmt.Fprintf(os.Stderr, "%sError Parsing Map: %v%s\n", Red, err, Reset)
 		return
 	}
 
 	paths, err := pathfinder.FindAllPaths(graph, cfg.Start, cfg.End)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%sError: %v%s\n",Red, err, Reset)
+		fmt.Fprintf(os.Stderr, "%sError: %v%s\n", Red, err, Reset)
 		return
 	}
 
@@ -38,9 +39,7 @@ func main() {
 
 	w := bufio.NewWriter(os.Stdout)
 	for _, turn := range turns {
-		w.WriteString(Magenta)
 		w.WriteString(turn)
-		w.WriteString(Reset)
 		w.WriteByte('\n')
 	}
 	w.Flush()
