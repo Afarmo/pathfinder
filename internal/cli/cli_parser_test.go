40b1e114ec25f62911b9c35b17d29ec5ce90d462
Abderahman Afaryad <kallahti@hotmail.com>
2026-07-29 20:57:14 +0300

    Fix cli_parser_test.go

--- internal/cli/cli_parser_test.go
+++ internal/cli/cli_parser_test.go
@@ -1,27 +1,28 @@
 package cli
-import(
+
+import (
 	"testing"
 )
 
-func TestCliParser(t *testing.T){
-	randomTest:= []struct{
+func TestCliParser(t *testing.T) {
+	randomTest := []struct {
 		name string
 		line []string
 		want bool
 	}{
-		{"valid", []string{"pwd","filePath.map", "start", "end", "5"}, false},
-		{"negative number of train", []string{"pwd","filePath.map", "start", "end", "not int"}, true},
-		{"negative number of train", []string{"pwd","filePath.map", "start", "end", "5", "some thing extra"}, true},
-		{"invalid extension", []string{"pwd","filePath", "start", "end", "5"}, true},
-		{"insufficient arguments", []string{"pwd","filePath.map", "end", "5"}, true},
-		{"same start and end station", []string{"pwd","filePath.map", "start", "start", "5"}, true},
-		{"negative number of train", []string{"pwd","filePath.map", "start", "end", "-5"}, true},
+		{"valid", []string{"pwd", "filePath.map", "start", "end", "5"}, false},
+		{"non integer train count", []string{"pwd", "filePath.map", "start", "end", "not int"}, true},
+		{"too many arguments", []string{"pwd", "filePath.map", "start", "end", "5", "some thing extra"}, true},
+		{"invalid extension", []string{"pwd", "filePath", "start", "end", "5"}, true},
+		{"too few arguments", []string{"pwd", "filePath.map", "end", "5"}, true},
+		{"same start and end station", []string{"pwd", "filePath.map", "start", "start", "5"}, true},
+		{"negative number of train", []string{"pwd", "filePath.map", "start", "end", "-5"}, true},
 	}
-	for _, testCases:= range randomTest{
-		t.Run(testCases.name, func(t *testing.T){
-			_, err:= ParseArgs(testCases.line)
-			if (err != nil) != testCases.want{
-				t.Errorf("wanted: %v, got: %v",testCases.want, err)
+	for _, testCases := range randomTest {
+		t.Run(testCases.name, func(t *testing.T) {
+			_, err := ParseArgs(testCases.line)
+			if (err != nil) != testCases.want {
+				t.Errorf("wanted: %v, got: %v", testCases.want, err)
 			}
 		})
 	}
