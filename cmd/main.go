5affd67972fb5ceecd209816943dad39830ad9a2
Yeabsira Abate Guangul <k-yguangu@c1r1p5.hive.fi>
2026-07-29 13:34:22 +0300

    add color

--- cmd/main.go
+++ cmd/main.go
@@ -11,22 +11,26 @@ import (
 	"pathfinder/internal/scheduler"
 )
 
+var Reset = "\033[0m"
+var Magenta = "\033[35m"
+var Red = "\033[31m"
+
 func main() {
 	cfg, err := cli.ParseArgs(os.Args)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error Parsing Arguments: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%sError Parsing Arguments: %v%s\n",Red, err, Reset)
 		return
 	}
 
 	graph, err := parser.MapParser(cfg.FilePath, cfg.Start, cfg.End)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error Parsing Map: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%sError Parsing Map: %v%s\n",Red, err, Reset)
 		return
 	}
 
 	paths, err := pathfinder.FindAllPaths(graph, cfg.Start, cfg.End)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%sError: %v%s\n",Red, err, Reset)
 		return
 	}
 
@@ -34,7 +38,7 @@ func main() {
 
 	w := bufio.NewWriter(os.Stdout)
 	for _, turn := range turns {
-		w.WriteString(turn)
+		w.WriteString(Magenta + turn + Reset)
 		w.WriteByte('\n')
 	}
 	w.Flush()
